74eaf0538ccc37b494b88865a3b7bfb29e84806a
yachibit <y.yachix@gmail.com>
2015-10-07 01:33:49 +0900

    Add immutable lists test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { List } from 'immutable';
 
 describe('immutability', () => {
 
@@ -14,6 +15,28 @@ describe('immutability', () => {
 
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
+    });
+  });
+
+  describe('a List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutabile', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
     });
   });
 });
