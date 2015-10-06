37650198371294043fe147e594a0f43a5799089e
yachibit <y.yachix@gmail.com>
2015-10-07 03:04:45 +0900

    Add logic for next

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
-import { setEntries } from '../src/core';
+import { setEntries, next } from '../src/core';
 
 describe('application logic', () => {
 
@@ -24,6 +24,24 @@ describe('application logic', () => {
       }));
     });
   
+  });
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+
+    });
+
   });
 
 });
